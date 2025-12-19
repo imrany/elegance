dbf02140e33c4fbd0e904337d4374997c5b220d5
imrany <imranmat254@gmail.com>
2025-12-19 15:09:54 +0300

    docs: update scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,6 +7,7 @@ docker stop elegance 2>/dev/null || true
 docker rm elegance 2>/dev/null || true
 docker rmi ghcr.io/imrany/elegance 2>/dev/null || true
 docker pull ghcr.io/imrany/elegance:latest
+chmod -R 777 ~/.elegance
 docker run -d --name elegance --env-file .env -p 8082:8082 -v ~/.elegance:/var/opt/elegance ghcr.io/imrany/elegance:latest
 echo "Deployment complete. Showing logs:"
 docker logs elegance
